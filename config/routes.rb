33ff674724d76799b174a8dfa1e2dd6573993907
Viet Pham <pxviet1997@gmail.com>
2024-12-26 11:25:04 +0700

    add friends

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :friends
   get "home/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -12,6 +13,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
-
-  get "home/about"
 end
